e6f4f60ee6a1888ef6910ed559e1af9cde7f9dd7
Raphael Sander <raphael.sander75@gmail.com>
2022-11-19 13:49:19 -0300

    feat: alias and autocomplete add

--- provision_k3s-master.sh
+++ provision_k3s-master.sh
@@ -13,3 +13,19 @@ file="/etc/hosts"
 
 # K3S Install
 curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server --cluster-init --write-kubeconfig-mode 644 --node-external-ip "192.168.0.50"
+
+# Alias and Auto Complete
+cat <<EOF >>/home/vagrant/.bashrc
+
+# kubectl autocomplete and alias
+# https://kubernetes.io/docs/reference/kubectl/cheatsheet/
+source <(kubectl completion bash)
+
+alias k=kubectl
+complete -o default -F __start_kubectl k
+
+# short alias to set/show context/namespace (only works for bash and bash-compatible shells, current context to be set before using kn to set namespace) 
+alias kx='f() { [ "$1" ] && kubectl config use-context $1 || kubectl config current-context ; } ; f'
+alias kn='f() { [ "$1" ] && kubectl config set-context --current --namespace $1 || kubectl config view --minify | grep namespace | cut -d" " -f6 ; } ; f'
+
+EOF
