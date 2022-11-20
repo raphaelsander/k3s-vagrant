025999deef74faeedf9f44b8ac03326dacdc76b5
Raphael Sander <raphael.sander75@gmail.com>
2022-11-20 13:52:12 -0300

    feat: whoami deploy add

--- provision_k3s-master.sh
+++ provision_k3s-master.sh
@@ -29,3 +29,6 @@ alias kx='f() { [ "$1" ] && kubectl config use-context $1 || kubectl config curr
 alias kn='f() { [ "$1" ] && kubectl config set-context --current --namespace $1 || kubectl config view --minify | grep namespace | cut -d" " -f6 ; } ; f'
 
 EOF
+
+# Whoami Deploy for Example
+/usr/local/bin/kubectl apply -f /vagrant/whoami.yaml
